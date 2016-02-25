2d7927f0fa55c1284572f5c8d648530afa9c5b07
falcopellegrino <franco.pellegrino@aruba.it>
2016-02-25 17:31:00 +0100

    test secondo commit

--- ProxyDesignPatternTester/Program.cs
+++ ProxyDesignPatternTester/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ProxyDesignPatternTester
 {
+    // Definition
+    // Provide a surrogate or placeholder for another object to control access to it. 
+
     class Program
     {
         static void Main(string[] args)
